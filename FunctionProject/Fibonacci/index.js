717f5aaf3f8967c16baf52759f8ce98106cb5f70
BJM19 <brayan.jimenez@mail.escuelaing.edu.co>
2021-11-28 15:54:18 -0500

    arreglos e imagenes

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -3,25 +3,29 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
+
     let answer = bigInt.zero;
 
     if (nth < 0)
         throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
+
     else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
-        }
+        answer = fiboMemo(nth)
+
     }
 
     context.res = {
         body: answer.toString()
     };
+}
+
+
+let memory = {0:bigInt.zero,1:bigInt.one};
+function fiboMemo(nth) {
+    if (nth in Object.keys(memory)) {
+        return memory[nth]
+    } else {
+        memory[nth] = fiboMemo(nth - 1).add(fiboMemo(nth - 2))
+        return memory[nth]
+    }
 }
